9f3d8267a2f827e8b22a8b90938310da2f23a42b
zakaria chahboun <zaki@localhost.localdomain>
2020-06-07 19:48:12 +0100

    first commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<!-- candy framework SASS - Global -->
+<style lang="scss" global>
+@import "./codyframe/assets/css/style.scss"
+</style>
+
+<!--<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>-->
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
+
